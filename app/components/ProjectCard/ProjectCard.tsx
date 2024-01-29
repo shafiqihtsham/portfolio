83b9ba61a690d5bf69aff135a4bec165489bb7c4
Ihtsham Shafiq <Ihtsham Shafiq>
2024-01-30 00:15:58 +1100

    added additional styling to home page

--- app/components/ProjectCard/ProjectCard.tsx
+++ app/components/ProjectCard/ProjectCard.tsx
@@ -42,7 +42,7 @@ const ProjectCard = (props: CardProps) => {
           </div>
         </div>
 
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3 [text-wrap:balance]">
           {props.description}
         </p>
         <div className="flex flex-row flex-nowrap">
