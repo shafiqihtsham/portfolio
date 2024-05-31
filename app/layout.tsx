4f929d32101fb8ddbb260aa457b6ef78289f0cca
Ihtsham Shafiq <Ihtsham123@outlook.com>
2024-05-31 21:28:35 +1000

    testing fouc changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Theme from "./provider";
+import { ThemeProvider } from "next-themes";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Theme from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ihtsham Shafiq",
-  description: "My portfolio",
+    title: "Ihtsham Shafiq",
+    description: "My portfolio",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className="min-h-screen min-w-screen"
-    >
-      <Theme>
-        <body className={inter.className}>
-          <Header />
-          {children} 
-          <Footer />
-        </body>
-      </Theme>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            suppressHydrationWarning
+            className="min-h-screen min-w-screen"
+        >
+            <body className={inter.className}>
+                <ThemeProvider defaultTheme="dark" enableSystem={false}>
+                    <Header />
+                    {children}
+                    <Footer />
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
