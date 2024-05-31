a226d31916cd6f5afbd82244138cfed322600247
Ihtsham Shafiq <Ihtsham123@outlook.com>
2024-05-31 21:12:36 +1000

    make provider server component

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,20 +1,13 @@
-"use client";
-
 import { ThemeProvider } from "next-themes";
-import { useState, useEffect } from "react";
 
 export default function Theme({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-  return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      {children}
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+        >
+            {children}
+        </ThemeProvider>
+    );
 }
