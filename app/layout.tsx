9dc4d273d893d65253e9e91526f6f0bacaf105d5
Ihtsham Shafiq <Ihtsham123@outlook.com>
2024-05-31 21:31:44 +1000

    revert broken deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,34 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import Theme from "./provider";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Theme from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Ihtsham Shafiq",
-    description: "My portfolio",
+  title: "Ihtsham Shafiq",
+  description: "My portfolio",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html
-            lang="en"
-            suppressHydrationWarning
-            className="min-h-screen min-w-screen"
-        >
-            <body className={inter.className}>
-                <ThemeProvider defaultTheme="dark" enableSystem={false}>
-                    <Header />
-                    {children}
-                    <Footer />
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className="min-h-screen min-w-screen"
+    >
+      <Theme>
+        <body className={inter.className}>
+          <Header />
+          {children} 
+          <Footer />
+        </body>
+      </Theme>
+    </html>
+  );
 }
