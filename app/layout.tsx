a274cd8e49a82f0bcac0911d2b3bb71d916f7979
Ihtsham Shafiq <Ihtsham Shafiq>
2024-06-04 17:33:07 +1000

    fuoc fix:update next-themes package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Theme from "./provider";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Ihtsham Shafiq",
@@ -22,13 +22,13 @@ export default function RootLayout({
       suppressHydrationWarning
       className="min-h-screen min-w-screen"
     >
-      <Theme>
-        <body className={inter.className}>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
           <Header />
-          {children} 
+          {children}
           <Footer />
-        </body>
-      </Theme>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
