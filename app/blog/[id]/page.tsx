a4777c936a830e9e0b58ac41dc3051ce9649984b
Ihtsham Shafiq <Ihtsham123@outlook.com>
2025-02-09 15:59:22 +1100

    fix: implement react-markdown library

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,19 +1,21 @@
 import { getPostData, getAllPostIds } from "@/lib/posts";
+import Markdown from "react-markdown";
+import Image from "next/image";
 
 type Params = Promise<{
   id: string;
 }>;
 
 type PostData = {
-  title: string;
   date: string;
-  contentHtml: string;
+  title: string;
+  id: string;
+  content: string;
 };
 
 export async function generateMetadata(props: { params: Params }) {
   const params = await props.params;
   const postData: PostData = await getPostData(params.id);
-
   return {
     title: postData.title,
   };
@@ -32,10 +34,22 @@ export default async function Post(props: { params: Params }) {
   return (
     <div className="container max-w-2xl mx-auto p-10 prose dark:prose-invert">
       <h1 className="font-extrabold text-3xl mb-10">{postData.title}</h1>
-      <div
-        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-        className="border-l-gray-300 dark:border-l-gray-700 border-l-1 pl-4 "
-      />
+      <div className="border-l-gray-300 dark:border-l-gray-700 border-l-1 pl-4 ">
+        <Markdown
+          components={{
+            img: (props) => (
+              <Image
+                src={props.src as string}
+                alt={props.alt as string}
+                width={1200}
+                height={200}
+              />
+            ),
+          }}
+        >
+          {postData.content}
+        </Markdown>
+      </div>
     </div>
   );
 }
